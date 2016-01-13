5ceb2b202242555ea94acb34cf3c31d0ebd76cf6
Pacher <pacher2016@gmail.com>
2016-01-13 13:44:23 +0600

    hotfix

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
     require('config/config.php');
-    if (!mysqli_connect('localhost', $user, $pass)) {
-        die('Database unavailable');
-    }
-
     require($path.'controllers/loginController.php');
     $controller = new loginController($path);
     $controller->loginPage();
